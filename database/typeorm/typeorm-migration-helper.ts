70e15819a283c5acfe2e42032e87f9cdc845d564
Jonatha Follmer <johnfollmer1995@gmail.com>
2026-03-06 06:02:14 -0300

    criacao do primeiro modulo de dominio

--- database/typeorm/typeorm-migration-helper.ts
+++ database/typeorm/typeorm-migration-helper.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import { ConfigService } from '@src/infra/module/config/service/config.service';
-import { TypeOrmMigrationService } from '@src/infra/module/typeorm/service/typeorm-migration.service';
-import { PersistenceModule } from '@src/persistence/persistence.module';
+import { TypeOrmMigrationService } from '@contentModule/infra/module/typeorm/service/typeorm-migration.service';
 import { DataSourceOptions } from 'typeorm';
 import { createPostgresDatabase } from 'typeorm-extension';
+import { PersistenceModule } from '@contentModule/persistence/persistence.module';
+import { ConfigService } from '@sharedModules/config/service/config.service';
 
 const createDatabaseModule = async () => {
   return await NestFactory.createApplicationContext(
